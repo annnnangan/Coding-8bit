b223506bd84dfc35f8bd654870fe4ee431198bc8
annnnangan <anna.ymngan@gmail.com>
2025-03-02 15:20:05 +0800

    feat: 導師預約時加入用戶登入/資料驗證

--- src/pages/user/auth/login.jsx
+++ src/pages/user/auth/login.jsx
@@ -12,6 +12,10 @@ const { VITE_API_BASE } = import.meta.env;
 export default function Login() {
   const navigate = useNavigate();
 
+  // 抓取路由上的search params
+  const [searchParams] = useSearchParams();
+  const redirectPath = searchParams?.get("redirect");
+
   // loading
   const [loadingState, setLoadingState] = useState(false);
 
@@ -25,7 +29,9 @@ export default function Login() {
         title: "登入成功",
         icon: "success",
       });
-      navigate("/");
+
+      const redirectTo = redirectPath ?? "/";
+      navigate(redirectTo);
     } catch (error) {
       Swal.fire({
         icon: "error",
@@ -49,8 +55,7 @@ export default function Login() {
         title: "驗證錯誤",
         text: error?.response?.data?.message,
       });
-      document.cookie =
-        "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     } finally {
       setLoadingState(false);
     }
@@ -64,25 +69,19 @@ export default function Login() {
   // 隱碼按鈕邏輯
   const [showPasswordList, setShowPasswordList] = useState([false, false]);
   const handleShowPassword = (index) => {
-    setShowPasswordList((prevState) =>
-      prevState.map((show, i) => (i === index ? !show : show))
-    );
+    setShowPasswordList((prevState) => prevState.map((show, i) => (i === index ? !show : show)));
   };
 
   // 初始化 - 確認是否有 token
   useEffect(() => {
-    const token =
-      document.cookie.replace(
-        /(?:(?:^|.*;\s*)authToken\s*=\s*([^;]*).*$)|^.*$/,
-        "$1"
-      ) || null;
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)authToken\s*=\s*([^;]*).*$)|^.*$/, "$1") || null;
     if (token) {
       loginCheck();
     }
   }, []);
 
   // 初始化 - 第三方登入確認身分
-  const [searchParams] = useSearchParams();
+
   const paramToken = searchParams.get("token");
   useEffect(() => {
     if (paramToken) {
@@ -90,11 +89,7 @@ export default function Login() {
 
       // 等待瀏覽器設置 Cookie
       setTimeout(() => {
-        const token =
-          document.cookie.replace(
-            /(?:(?:^|.*;\s*)authToken\s*=\s*([^;]*).*$)|^.*$/,
-            "$1"
-          ) || null;
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)authToken\s*=\s*([^;]*).*$)|^.*$/, "$1") || null;
 
         if (token) {
           loginCheck(token);
@@ -115,10 +110,7 @@ export default function Login() {
         <div className="container">
           <NavLink to="/" className="navbar-brand">
             <picture>
-              <source
-                srcSet="images/logo-sm.svg"
-                media="(max-width: 575.98px)"
-              />
+              <source srcSet="images/logo-sm.svg" media="(max-width: 575.98px)" />
               <img src="images/logo.svg" alt="logo-image" />
             </picture>
           </NavLink>
@@ -126,23 +118,11 @@ export default function Login() {
             <div className="col-lg-6">
               <div className="user-auth-card card border-0 rounded-2">
                 <div className="card-body px-6 py-10 p-lg-13">
-                  <h1 className="fs-4 fs-lg-3 text-brand-03">
-                    Coding∞bit 會員登入
-                  </h1>
+                  <h1 className="fs-4 fs-lg-3 text-brand-03">Coding∞bit 會員登入</h1>
                   <form className="mt-6 mt-lg-11">
                     <div className="position-relative">
-                      <input
-                        type="email"
-                        className="form-control underline-input"
-                        id="email"
-                        name="email"
-                        aria-describedby="emailHelp"
-                        placeholder="請輸入電子信箱"
-                        onChange={handleInputChange}
-                      />
-                      <span className="material-symbols-outlined position-absolute top-0 text-gray-03 ms-1 mt-1">
-                        mail
-                      </span>
+                      <input type="email" className="form-control underline-input" id="email" name="email" aria-describedby="emailHelp" placeholder="請輸入電子信箱" onChange={handleInputChange} />
+                      <span className="material-symbols-outlined position-absolute top-0 text-gray-03 ms-1 mt-1">mail</span>
                     </div>
                     <div className="position-relative mt-9">
                       <input
@@ -153,9 +133,7 @@ export default function Login() {
                         placeholder="請輸入密碼"
                         onChange={handleInputChange}
                       />
-                      <span className="material-symbols-outlined position-absolute top-0 text-gray-03 ms-1 mt-1">
-                        lock
-                      </span>
+                      <span className="material-symbols-outlined position-absolute top-0 text-gray-03 ms-1 mt-1">lock</span>
                       <a
                         href="#"
                         onClick={(e) => {
@@ -163,45 +141,23 @@ export default function Login() {
                           handleShowPassword(0);
                         }}
                       >
-                        <span className="material-symbols-outlined position-absolute top-0 end-0 text-gray-03 ms-1 mt-1">
-                          {showPasswordList[0]
-                            ? "visibility"
-                            : "visibility_off"}
-                        </span>
+                        <span className="material-symbols-outlined position-absolute top-0 end-0 text-gray-03 ms-1 mt-1">{showPasswordList[0] ? "visibility" : "visibility_off"}</span>
                       </a>
                     </div>
                     <div className="f-between-center mt-9">
                       <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value=""
-                          id="flexCheckDefault"
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="flexCheckDefault"
-                        >
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexCheckDefault">
                           記住帳號
                         </label>
                       </div>
-                      <NavLink
-                        to="/forgot-password"
-                        className="link-brand-03 fw-medium underline-hover link-hover"
-                      >
+                      <NavLink to="/forgot-password" className="link-brand-03 fw-medium underline-hover link-hover">
                         忘記密碼
                       </NavLink>
                     </div>
-                    <button
-                      type="button"
-                      className="btn btn-brand-03 rounded-2 slide-right-hover w-100 f-center mt-4 mt-lg-6"
-                      id="loginBtn"
-                      onClick={loginFn}
-                    >
+                    <button type="button" className="btn btn-brand-03 rounded-2 slide-right-hover w-100 f-center mt-4 mt-lg-6" id="loginBtn" onClick={loginFn}>
                       立即登入
-                      <span className="material-symbols-outlined icon-fill fs-6 fs-md-5 mt-1 ms-1">
-                        arrow_forward
-                      </span>
+                      <span className="material-symbols-outlined icon-fill fs-6 fs-md-5 mt-1 ms-1">arrow_forward</span>
                     </button>
                   </form>
                   <div className="divider-label d-flex align-items-center mt-6 mt-lg-8">
@@ -209,24 +165,13 @@ export default function Login() {
                     <span>OR</span>
                     <hr />
                   </div>
-                  <a
-                    href={`${VITE_API_BASE}/auth/google`}
-                    type="button"
-                    className="btn btn-brand-02 border-1 rounded-1 w-100 f-center mt-6 mt-lg-8"
-                  >
-                    <img
-                      src="images/icon/icons-google.svg"
-                      alt="icon-google"
-                      className="me-3"
-                    />
+                  <a href={`${VITE_API_BASE}/auth/google`} type="button" className="btn btn-brand-02 border-1 rounded-1 w-100 f-center mt-6 mt-lg-8">
+                    <img src="images/icon/icons-google.svg" alt="icon-google" className="me-3" />
                     使用 Google 登入
                   </a>
                   <div className="f-end-center mt-6 mt-lg-8">
                     <p className="text-center">還不是會員？</p>
-                    <NavLink
-                      to="/signup"
-                      className="link-brand-03 fw-medium underline-hover ms-1"
-                    >
+                    <NavLink to="/signup" className="link-brand-03 fw-medium underline-hover ms-1">
                       點此註冊
                     </NavLink>
                   </div>
